996dc08a57af339a742e935d6785fb2b20d42444
Nguyenhuy9981 <vuongsieu9981@gmail.com>
2023-02-23 21:10:16 +0700

    cập nhật model Job

--- src/app/models/Job.js
+++ src/app/models/Job.js
@@ -8,11 +8,30 @@ const Job = new Schema({
     maxLength: 255,
     require: true,
   },
+
   status: {
     type: String,
     enum: ['unfulfilled', 'processing', 'done'],
-    default: 'unfinish',
+    default: 'unfulfilled',
   },
+
+  checkBox: [{
+    name: {
+      type: String,
+      maxLength: 255,
+      require: true,
+    },
+    status: {
+      type: String,
+      enum: ['unfulfilled', 'processing', 'done'],
+      default: 'unfulfilled',
+    },
+  }],
+
+  file: {
+    type: String,
+  },
+
   authorRef: {
     type: Schema.Types.ObjectId,
     ref: 'User',
