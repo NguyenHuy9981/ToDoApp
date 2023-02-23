b88da3d06820a1eee598070b04f563f40942ef4e
Nguyenhuy9981 <vuongsieu9981@gmail.com>
2023-02-23 09:58:21 +0700

    todo

---
+++ app/Controller/ToDoController.js
@@ -0,0 +1,40 @@
+const Job = require('../models/Job');
+
+
+class ToDoController {
+
+    index(req, res, next) {
+        Job.find({ $or: [{ status: "unfinish" }, { status: "finish" }] })
+            .then(job =>
+                res.status(200).json(job)
+            )
+            .catch(res.status(500))
+
+    }
+
+
+    create(req, res, next) {
+        const filter = { name: req.body.name }
+        const update = { status: 'unfinish' }
+        const options = { upsert: true, new: true, setDefaultsOnInsert: true }
+
+        Job.findOneAndUpdate(filter, update, options)
+            .then(job =>
+                res.status(200).json(job)
+            )
+            .catch(res.status(500))
+    }
+
+
+    update(req, res, next) {
+        Job.updateOne({ _id: req.params.id }, req.body)
+            .then(
+                res.send('Update Successfully')
+            )
+            .catch(res.status(500))
+    }
+
+
+}
+
+module.exports = new ToDoController
---
+++ app/models/Job.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Job = new mongoose.Schema({
+    name: { 
+        type: String, 
+        maxLength: 255 
+    },
+    status: {
+        type: String,
+        enum : ['unfinish','finish','disable'],
+        default: 'unfinish'
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Job', Job);
