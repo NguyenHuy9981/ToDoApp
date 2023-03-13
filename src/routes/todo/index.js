bfa047701c2ddd6d344b9f5b3b4567312c80ac8b
Huy <vuongsieu9981@gmail.com>
2023-03-13 18:20:22 +0700

    route clean

--- src/routes/todo/index.js
+++ src/routes/todo/index.js
@@ -3,14 +3,16 @@ const ToDoController = require('../../app/controller/ToDoController');
 
 const router = express.Router();
 
-router.get('/', ToDoController.index);
 router.get('/stats', ToDoController.stats);
+
+router.get('/', ToDoController.index);
 router.post('/create', ToDoController.create);
 router.get('/:id', ToDoController.detail);
 router.put('/:id', ToDoController.update);
+router.delete('/:id', ToDoController.delete);
+
 router.put('/addFile/:id', ToDoController.addFile);
 router.put('/removeFile/:id', ToDoController.removeFile);
-router.delete('/:id', ToDoController.delete);
 
 router.use('/comment', require('./comment'));
 router.use('/file', require('./file'));
